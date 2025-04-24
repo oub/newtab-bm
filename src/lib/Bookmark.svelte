f6b6169007f34a53541cd564b050214a5a4c624c
Olivier CUENOT <olivier-cuenot@samse.fr>
2025-04-24 10:08:53 +0200

    feat: Refactor Bookmark component to use Favicon directly and remove unused image loading logic

--- src/lib/Bookmark.svelte
+++ src/lib/Bookmark.svelte
@@ -1,5 +1,6 @@
 <script module>
   import { type Bookmarks } from "webextension-polyfill-ts";
+  import Favicon from "./Favicon.svelte";
   import Hoverable from "./Hoverable.svelte";
   import Item from "./Item.svelte";
   import { settings } from "./settings";
@@ -11,8 +12,6 @@
 
 <script lang="ts">
   let { bookmark }: Props = $props();
-  let errorOnFirstImgLoad = $state(false);
-  let errorOnSecondImgLoad = $state(false);
 
   const labelMode = $derived.by(() => {
     switch ($settings.labelMode) {
@@ -26,68 +25,15 @@
         return "never";
     }
   });
-
-  const googMaps = $derived(
-    bookmark.url?.startsWith("https://www.google.fr/maps/") ||
-      bookmark.url?.startsWith("https://www.google.com/maps/")
-      ? bookmark.url
-      : undefined
-  );
-  const imgSrc = $derived(
-    errorOnFirstImgLoad
-      ? undefined
-      : googMaps
-        ? "https://www.google.com/s2/favicons?domain_url=maps.google.com&sz=32"
-        : bookmark.favicon
-  );
-  const fallbackImageURL = $derived.by(() => {
-    const hostname = new URL(bookmark.url ?? "").hostname;
-    return `https://icon.horse/icon/${hostname}`;
-    // https://www.google.com/s2/favicons?domain_url={hostname}&sz=32
-  });
 </script>
 
 <Hoverable>
   <a href={bookmark.url}>
-    <!-- <Item
-      imgSrc={`${bookmark.url}favicon.ico`}
-      label={bookmark.title}
-      showLabelOnlyOnHover={!showBookmarkLabels}
-    /> -->
-    <Item
-      {imgSrc}
-      label={bookmark.title}
-      {labelMode}
-      children={errorOnFirstImgLoad ? fallbackImage : undefined}
-    />
+    <Item label={bookmark.title} {labelMode}>
+      <Favicon {bookmark} />
+    </Item>
   </a>
 </Hoverable>
-
-<img
-  src={bookmark.favicon}
-  alt=""
-  class="img-loader"
-  onerror={() => (errorOnFirstImgLoad = true)}
-/>
-
-{#snippet fallbackImage()}
-  {#if errorOnFirstImgLoad}
-    <!-- {#if !errorOnSecondImgLoad}
-      <img
-        src={fallbackImageURL}
-        alt=""
-        style:border-radius="4px"
-        onerror={() => (errorOnSecondImgLoad = true)}
-      />
-    {:else if errorOnSecondImgLoad} -->
-    <img
-      src="chrome://branding/content/about-logo.png"
-      alt=""
-      style="filter:grayscale(1) opacity(0.15)"
-    />
-    <!-- {/if} -->
-  {/if}
-{/snippet}
 
 <style>
   a {
@@ -99,14 +45,5 @@
         outline: solid 2px dodgerblue;
       }
     }
-
-    img {
-      height: 32px;
-      width: 32px;
-    }
-  }
-
-  .img-loader {
-    display: none;
   }
 </style>
