499ed605dd819f2573608c4f6802e999be710456
Olivier CUENOT <olivier-cuenot@samse.fr>
2025-05-03 10:59:10 +0200

    refactor: Simplify retrieveColors function by using async/await for better readability

--- webapp/src/lib/colors.ts
+++ webapp/src/lib/colors.ts
@@ -14,37 +14,36 @@ export const getBorderColor = (
 	backgroundColor: string
 ): string => `color-mix(in srgb, ${foregroundColor} 40%, ${backgroundColor})`;
 
-export const retrieveColors = (updateInfo?: Theme.ThemeUpdateInfo) => {
+export const retrieveColors = async (updateInfo?: Theme.ThemeUpdateInfo) => {
 	if (Browser.theme) {
 		// This only works in Firefox
-		return Browser.theme.getCurrent().then((theme) => {
-			let backgroundColor: string =
-				(theme.colors?.popup as string) ?? defaultLightColor;
-			let foregroundColor: string =
-				(theme.colors?.popup_text as string) ?? defaultDarkColor;
+		const theme = await Browser.theme.getCurrent();
+		let backgroundColor: string =
+			(theme.colors?.popup as string) ?? defaultLightColor;
+		let foregroundColor: string =
+			(theme.colors?.popup_text as string) ?? defaultDarkColor;
 
-			// Convert colors to computed values
-			foregroundColor = convertToComputedColor(foregroundColor);
-			backgroundColor = convertToComputedColor(backgroundColor);
+		// Convert colors to computed values
+		foregroundColor = convertToComputedColor(foregroundColor);
+		backgroundColor = convertToComputedColor(backgroundColor);
 
-			let borderColor = getBorderColor(foregroundColor, backgroundColor);
+		let borderColor = getBorderColor(foregroundColor, backgroundColor);
 
-			// Sometimes, theme.getCurrent() returns null, so we need to estimate the color scheme
-			let scheme: ColorScheme =
-				getDarkestColor(backgroundColor, foregroundColor) === backgroundColor
-					? 'dark'
-					: 'light';
+		// Sometimes, theme.getCurrent() returns null, so we need to estimate the color scheme
+		let scheme: ColorScheme =
+			getDarkestColor(backgroundColor, foregroundColor) === backgroundColor
+				? 'dark'
+				: 'light';
 
-			if (scheme === 'light') {
-				backgroundColor = getDarkestColor(backgroundColor, defaultLightColor);
-			} else {
-				backgroundColor = getLightestColor(backgroundColor, defaultDarkColor);
-			}
+		if (scheme === 'light') {
+			backgroundColor = getDarkestColor(backgroundColor, defaultLightColor);
+		} else {
+			backgroundColor = getLightestColor(backgroundColor, defaultDarkColor);
+		}
 
-			colorScheme.set(scheme);
+		colorScheme.set(scheme);
 
-			return { backgroundColor, foregroundColor, borderColor };
-		});
+		return { backgroundColor, foregroundColor, borderColor };
 	} else {
 		// Error retrieving colors. Falling back to defaults
 		return Promise.resolve({
