57db3b5899e2f1899b496fae6ee6e9e382baa6d1
Olivier CUENOT <olivier-cuenot@samse.fr>
2025-05-03 10:54:34 +0200

    refactor: Remove artifactsDir and build configuration from .web-ext-config.mjs for simplification

--- .web-ext-config.mjs
+++ .web-ext-config.mjs
@@ -1,14 +1,8 @@
 export default {
 	verbose: false,
-	artifactsDir: './releases',
-	build: {
-		overwriteDest: true,
-		filename: '{name}-{version}.xpi',
-	},
 	ignoreFiles: [
 		'addons.mozilla.json',
 		'*config.*',
-		'index.html',
 		'node_modules',
 		'package*.json',
 		'public',
